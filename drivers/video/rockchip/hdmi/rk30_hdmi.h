d21b673368853d809234d1d4ca3af8f225a5f648
lbdroid <definesinsanity@gmail.com>
2013-04-29 11:08:07 -0300

    Set default video mode based on panel native resolution from EDID.

--- drivers/video/rockchip/hdmi/rk30_hdmi.h
+++ drivers/video/rockchip/hdmi/rk30_hdmi.h
@@ -35,9 +35,8 @@ enum {
 /* If HDMI_ENABLE, system will auto configure output mode according to EDID 
  * If HDMI_DISABLE, system will output mode according to macro HDMI_VIDEO_DEFAULT_MODE
  */
-// Omegemoon: HDMI "black screen" issue
-//#define HDMI_AUTO_CONFIGURE			HDMI_ENABLE
-#define HDMI_AUTO_CONFIGURE			HDMI_DISABLE
+#define HDMI_AUTO_CONFIGURE			HDMI_ENABLE
+//#define HDMI_AUTO_CONFIGURE			HDMI_DISABLE
 
 /* default HDMI output video mode */
 #if defined(CONFIG_HDMI_RK30_RES_1080P)
